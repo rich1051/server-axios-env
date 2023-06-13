97aafd08453aa594b80fa3fb32d469d2d730b287
Will Richardson <rich1051@umn.edu>
2023-06-13 16:40:53 -0500

    Lecture completed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
+const axios = require("axios");
+require('dotenv').config();
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
+/** ---------- API ROUTE ---------- */
+// listen for GET request from client at /gifs
+app.get("/gifs", (req, res) => {
+// when client sends request, use axios to send GET request to Giphy API
+  axios
+    .get(
+      `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&rating=pg`
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log("There was an error GETTING", error);
+    });
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log("Listening on port: ", PORT);
 });
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,36 @@
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
-import axios from 'axios';
-import './App.css';
+function App() {
+  const [gifs, setGifs] = useState([]);
 
-function App () {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">APIS</h1>
-          <h4><i>APIS</i></h4>
-        </header>
-        <br/>
-      </div>
-    );
-  
+  const handleSubmit = () => {
+    axios
+      .get("/gifs")
+      .then((response) => {
+        console.log(response.data.data);
+        setGifs(response.data.data);
+      })
+      .catch((error) => {
+        console.log("Error GETTING gifs in app", error);
+      });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">APIS</h1>
+        <h4>
+          <i>APIS</i>
+        </h4>
+      </header>
+      <button onClick={handleSubmit}>Gifs Pls</button>
+      {gifs.map((gif, i) => (
+        <img key={i} src={gif.images.original.url}></img>
+      ))}
+      <br />
+    </div>
+  );
 }
 
 export default App;
